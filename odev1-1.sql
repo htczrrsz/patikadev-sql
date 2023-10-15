280d2ca06b2ea75aeba57cd76f283b19b29121a7
Hatice <128534671+htczrrsz@users.noreply.github.com>
2023-10-14 22:39:59 -0500

    Update odev1-1.sql

--- odev1-1.sql
+++ odev1-1.sql
@@ -5,4 +5,6 @@ SELECT title, description FROM film;
 SELECT * FROM film
 WHERE length > 60 AND length < 70;
 
-
+-- 3.soru
+SELECT * FROM film 
+WHERE rental_rate=0.99 AND replacement_cost=12.99 OR replacement_cost=28.99;
